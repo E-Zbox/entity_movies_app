84e75c590c9495977c21ace69ea923a2ee27ce7d
E-Zbox <zboxstudios97@gmail.com>
2023-01-12 15:59:58 +0100

    deleted
    main entry point to this backend service
    
    this handles the user resolver logic and controllers for creating, logging in user

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,67 @@
+// dependencies
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { mergeResolvers, mergeTypeDefs } = require("@graphql-tools/merge");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { join } = require("path");
+// models
+const models = require("../models");
+// resolvers
+const episodeResolver = require("../resolvers/episodeResolver");
+const genreResolver = require("../resolvers/genreResolver");
+const movieResolver = require("../resolvers/movieResolver");
+const seasonResolver = require("../resolvers/seasonResolver");
+const seriesResolver = require("../resolvers/seriesResolver");
+const resolvers = mergeResolvers([
+    episodeResolver,
+    genreResolver,
+    movieResolver,
+    seasonResolver,
+    seriesResolver,
+]);
+// typeDefs
+const episodeTypeDefs = loadSchemaSync(
+    join(__dirname, "../schema/Episode.graphql"),
+    { loaders: [new GraphQLFileLoader()] }
+);
+const genreTypeDefs = loadSchemaSync(
+    join(__dirname, "../schema/Genre.graphql"),
+    { loaders: [new GraphQLFileLoader()] }
+);
+const movieTypeDefs = loadSchemaSync(
+    join(__dirname, "../schema/Movie.graphql"),
+    { loaders: [new GraphQLFileLoader()] }
+);
+const seasonTypeDefs = loadSchemaSync(
+    join(__dirname, "../schema/Season.graphql"),
+    { loaders: [new GraphQLFileLoader()] }
+);
+const seriesTypeDefs = loadSchemaSync(
+    join(__dirname, "../schema/Series.graphql"),
+    { loaders: [new GraphQLFileLoader()] }
+);
+const typeDefs = mergeTypeDefs([
+    episodeTypeDefs,
+    genreTypeDefs,
+    movieTypeDefs,
+    seasonTypeDefs,
+    seriesTypeDefs,
+]);
+// schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+// utils
+const modelsUtils = require("../utils/models");
+
+exports.context = {
+    models,
+    utils: {
+        ...modelsUtils,
+    },
+};
+
+exports.GQLController = graphqlHTTP({
+    context: this.context,
+    graphiql: false,
+    schema,
+});
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,7 +1,31 @@
+// context
+const { context: _context } = require("./index");
 // env
 const {
     env: { BCRYPT_SALT, JWT_SECRET_KEY },
 } = process;
+// modular imports
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { join } = require("path");
+// resolvers
+const resolvers = require("../resolvers/userResolver");
+
+const typeDefs = loadSchemaSync(join(__dirname, "../schema/User.graphql"), {
+    loaders: [new GraphQLFileLoader()],
+});
+// schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+// utils
+const userUtils = require("../utils/models/user");
+
+const context = {
+    ..._context,
+    env: { BCRYPT_SALT, JWT_SECRET_KEY },
+    utils: { ...userUtils, ..._context.utils },
+};
 
 const {
     createUser,
@@ -9,7 +33,13 @@ const {
     comparePassword,
     generateToken,
     verifyToken,
-} = require("../utils/models/user");
+} = userUtils;
+
+exports.userGQLController = graphqlHTTP((req, res, next) => ({
+    context: { ...context, req, res, next },
+    graphiql: false,
+    schema,
+}));
 
 exports.createUserController = async (req, res, next) => {
     try {
@@ -59,7 +89,28 @@ exports.loginUserController = async (req, res, next) => {
             email: user.email,
             password: user.password,
         };
-        let token = generateToken(payload, JWT_SECRET_KEY);
+
+        let token =
+            user.logged_in_token || generateToken(payload, JWT_SECRET_KEY);
+
+        // verifies token to check if token has expired
+        try {
+            verifyToken(token, JWT_SECRET_KEY);
+        } catch (error) {
+            if (error.name === "TokenExpiredError") {
+                token = generateToken(payload, JWT_SECRET_KEY);
+            }
+        }
+
+        if (token !== user.logged_in_token) {
+            let {
+                models: { User },
+            } = context;
+            await User.updateOne(
+                { email: user.email },
+                { logged_in_token: token }
+            );
+        }
 
         return res.status(200).json({
             success: true,
