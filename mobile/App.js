724dd0233860c81bc817d5b56efb27608debd633
E-Zbox <zboxstudios97@gmail.com>
2023-01-17 04:52:36 +0100

    Finished installing react-native and checked if everything was working properly

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// asyncStorage
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// expo
+import {
+    useFonts,
+    Roboto_300Light,
+    Roboto_900Black,
+} from "@expo-google-fonts/roboto";
+import { StatusBar } from "expo-status-bar";
+// navigator
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// react
+import React from "react";
+import { Alert, Text, TextInput, View } from "react-native";
+// routes
+import UserAuthNavigator from "./navigator/UserAuthNavigator";
+// screens
+// styled
+import { ThemeProvider } from "styled-components/native";
+// styles
+import { AppContainer } from "./styles/App";
+import { MainContainer } from "./styles/shared/Container";
+// utils
+import { theme } from "./utils/data";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    const [fontsLoaded] = useFonts({ Roboto_300Light, Roboto_900Black });
+
+    return (
+        <ThemeProvider theme={theme}>
+            {fontsLoaded ? (
+                <NavigationContainer
+                // onLayout={() => {Alert.alert("onLayout Prop on Image Called....");}}
+                >
+                    <Stack.Navigator screenOptions={{ headerShown: false }}>
+                        <Stack.Screen
+                            name="UserAuthNavigator"
+                            component={UserAuthNavigator}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            ) : (
+                <AppContainer>
+                    <Text style={{ fontSize: 32, color: "#66ccee" }}>
+                        Loading...
+                    </Text>
+                </AppContainer>
+            )}
+            <StatusBar />
+        </ThemeProvider>
+    );
+};
+
+export default App;
